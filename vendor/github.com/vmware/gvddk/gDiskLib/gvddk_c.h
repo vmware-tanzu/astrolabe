16517d3d42fed4cc059438bb85c9f0bf3603cc46
Dave Smith-Uchida <dsmithuchida@vmware.com>
2019-12-16 12:55:01 -0800

    Merged gvddk from govmomi->astrolabe project.  Changed path from vmware/govmomi to vmware/gvddk Added instructions on installation

---
+++ vendor/github.com/vmware/gvddk/gDiskLib/gvddk_c.h
@@ -0,0 +1,45 @@
+/*
+Copyright (c) 2018-2019 the gvddk contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "vixDiskLib.h"
+
+typedef struct {
+    VixDiskLibHandle dli;
+    VixError err;
+} DiskHandle;
+
+void LogFunc(const char *fmt, va_list args);
+void GoLogWarn(char * msg);
+VixError Init(uint32 major, uint32 minor, char* libDir);
+VixError Connect(VixDiskLibConnectParams *cnxParams, VixDiskLibConnection *connection);
+VixError ConnectEx(VixDiskLibConnectParams *cnxParams, bool readOnly, char* transportModes, VixDiskLibConnection *connection);
+DiskHandle Open(VixDiskLibConnection conn, char* path, uint32 flags);
+VixError PrepareForAccess(VixDiskLibConnectParams *cnxParams, char* identity);
+void Params_helper(VixDiskLibConnectParams *cnxParams, char* arg1, char* arg2, char* arg3, bool isFcd, bool isSession);
+VixError Create(VixDiskLibConnection connection, char *path, VixDiskLibCreateParams *createParams, void *progressCallbackData);
+bool ProgressFunc(void *progressData, int percentCompleted);
+VixError CreateChild(VixDiskLibHandle diskHandle, char *childPath, VixDiskLibDiskType diskType, void *progressCallbackData);
+VixError Defragment(VixDiskLibHandle diskHandle, void *progressCallbackData);
+VixError GetInfo(VixDiskLibHandle diskHandle, VixDiskLibInfo *info);
+VixError Grow(VixDiskLibConnection connection, char* path, VixDiskLibSectorType capacity, bool updateGeometry, void *progressCallbackData);
+VixError Shrink(VixDiskLibHandle diskHandle, void *progressCallbackData);
+VixError CheckRepair(VixDiskLibConnection connection, char *file, bool repair);
+VixError Cleanup(VixDiskLibConnectParams *connectParams, uint32 numCleanedUp, uint32 numRemaining);
+VixError GetMetadataKeys(VixDiskLibHandle diskHandle, char *buf, size_t bufLen, size_t required);
+VixError Clone(VixDiskLibConnection dstConn, char *dstPath, VixDiskLibConnection srcConn, char *srcPath, VixDiskLibCreateParams *createParams,
+               void *progressCallbackData, bool overWrite);
---
+++ vendor/github.com/vmware/gvddk/test/low_level_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/vmware/gvddk/gDiskLib"
+	"os"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	// Set up
+	path := os.Getenv("LIBPATH")
+	if path == "" {
+		t.Skip("Skipping testing if environment variables are not set.")
+	}
+	res := gDiskLib.Init(6, 7, path)
+	if res != nil {
+		t.Errorf("Init failed, got error code: %d, error message: %s.", res.VixErrorCode(), res.Error())
+	}
+	serverName := os.Getenv("IP")
+	thumPrint := os.Getenv("THUMBPRINT")
+	userName := os.Getenv("USERNAME")
+	password := os.Getenv("PWD")
+	fcdId := os.Getenv("FCDID")
+	ds := os.Getenv("DATASTORE")
+	identity := os.Getenv("IDENTITY")
+	params := gDiskLib.NewConnectParams("", serverName,thumPrint, userName,
+		password, fcdId, ds, "", "", identity, "", gDiskLib.VIXDISKLIB_FLAG_OPEN_COMPRESSION_SKIPZ,
+		false, gDiskLib.NBD)
+	err1 := gDiskLib.PrepareForAccess(params)
+	if err1 != nil {
+		t.Errorf("Prepare for access failed. Error code: %d. Error message: %s.", err1.VixErrorCode(), err1.Error())
+	}
+	conn, err2 := gDiskLib.ConnectEx(params)
+	if err2 != nil {
+		gDiskLib.EndAccess(params)
+		t.Errorf("Connect to vixdisk lib failed. Error code: %d. Error message: %s.", err2.VixErrorCode(), err2.Error())
+	}
+	gDiskLib.Disconnect(conn)
+	gDiskLib.EndAccess(params)
+}
